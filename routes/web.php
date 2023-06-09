f3573302a76214215ff0b11a2685e5c27d0c6829
femas <111944157+femas66@users.noreply.github.com>
2023-06-09 14:57:07 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//Login
 Route::get('/login', [LoginController::class, 'index'])->name('login.index');
 Route::post('/login', [LoginController::class, 'store'])->name('login.store');
