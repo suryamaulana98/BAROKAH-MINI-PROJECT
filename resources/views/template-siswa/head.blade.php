606cd01270c799c0e3d69141f203149e3bd093a4
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-06-11 20:19:42 +0700

    menambahkan template setiap bagian

---
+++ resources/views/template-siswa/head.blade.php
@@ -0,0 +1,104 @@
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<title>HumaRulles</title>
+<meta content="" name="description">
+<meta content="" name="keywords">
+
+<!-- Favicons -->
+<link href="assets/img/favicon.png" rel="icon">
+<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+<!-- Google Fonts -->
+<link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+<script src="https://kit.fontawesome.com/a166b9accd.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-..." crossorigin="anonymous" />
+
+
+<!-- Template Main CSS File -->
+<link href="assets/css/style.css" rel="stylesheet">
+<!-- <link href="assets/css/peraturan.css" rel="stylesheet"> -->
+<!-- =======================================================
+* Template Name: Vesperr
+* Updated: May 30 2023 with Bootstrap v5.3.0
+* Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+======================================================== -->
+<style>
+    .preloader {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        position: fixed;
+        background-color: white;
+        z-index: 9999;
+    }
+
+    .loading {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+    }
+
+    .pengumuman ul li a {
+        text-decoration: none;
+    }
+
+    .connector {
+        position: absolute;
+        width: 20px;
+        /* Sesuaikan dengan lebar yang diinginkan */
+        height: 20px;
+        /* Sesuaikan dengan tinggi yang diinginkan */
+        margin-right: 10px;
+        /* Jarak antara simbol bulat dan teks */
+        margin-left: -30px;
+        margin-top: 100px;
+    }
+
+    .connector::before {
+        content: '';
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 16px;
+        /* Ukuran diameter bulatan */
+        height: 16px;
+        /* Ukuran diameter bulatan */
+        border: 2px solid #0563bb;
+        /* Warna dan ketebalan border */
+        border-radius: 50%;
+        /* Membuat simbol bulat */
+    }
+
+    .connector::after {
+        content: '';
+        position: absolute;
+        top: calc(50% + 8px);
+        /* Jarak garis vertikal dari bulatan */
+        left: 50%;
+        transform: translateX(-50%);
+        width: 2px;
+        /* Lebar garis vertikal */
+        height: 100%;
+        /* Tinggi garis vertikal */
+        background-color: #0563bb;
+        /* Warna garis vertikal */
+    }
+</style>
