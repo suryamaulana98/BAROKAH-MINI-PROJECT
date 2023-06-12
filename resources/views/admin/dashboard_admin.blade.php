fde6c98f982ef4ac462605b4c3174600afa86920
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-06-12 14:24:34 +0700

    oke

--- resources/views/admin/dashboard_admin.blade.php
+++ resources/views/admin/dashboard_admin.blade.php
@@ -386,7 +386,7 @@
 
                 <footer class="footer pt-3  ">
                     <div class="container-fluid">
-                        <div class="row align-items-center justify-content-lg-between">
+                        <div class="row align-items-center justify-content-lg-center">
                             <div class="col-lg-6 mb-lg-0 mb-4">
                                 <div class="copyright text-center text-sm text-muted text-lg-start">
                                     ©
