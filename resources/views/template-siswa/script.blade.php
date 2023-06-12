af431bf62d1446a2b21fc649d9a560c474b7c38e
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-06-12 10:39:57 +0700

    menambahkan perubahan pada halaman siswa

---
+++ resources/views/template-siswa/script.blade.php
@@ -0,0 +1,11 @@
+<!-- Vendor JS Files -->
+<script src="/siswa/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="/siswa/assets/vendor/aos/aos.js"></script>
+<script src="/siswa/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/siswa/assets/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/siswa/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="/siswa/assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/siswa/assets/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/siswa/assets/js/main.js"></script>
