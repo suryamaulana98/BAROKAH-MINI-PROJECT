b8b1a0cb7dc60b651dde1baa6a1574fdc7eeb3ec
femas <111944157+femas66@users.noreply.github.com>
2023-06-12 11:39:00 +0700

    include admin

---
+++ resources/views/template-admin/head.blade.php
@@ -0,0 +1,11 @@
+<!--     Fonts and icons     -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+<link rel="icon" type="image/png" href="/admin/assets/img/icons/logo.png">
+<!-- Nucleo Icons -->
+<link href="/admin/assets/css/nucleo-icons.css" rel="stylesheet" />
+<link href="/admin/assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- Font Awesome Icons -->
+<script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+<link href="/admin/assets/css/nucleo-svg.css" rel="stylesheet" />
+<!-- CSS Files -->
+<link id="pagestyle" href="/admin/assets/css/argon-dashboard.css?v=2.0.4" rel="stylesheet" />
