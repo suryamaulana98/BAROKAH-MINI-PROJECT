6dec91c0fa14ee57c117e09080d3b80ccfa6922d
femas <111944157+femas66@users.noreply.github.com>
2023-06-13 12:27:46 +0700

    absen admin

--- resources/views/admin/list_siswa.blade.php
+++ resources/views/admin/list_siswa.blade.php
@@ -129,10 +129,10 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
                         <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white"
-                                href="javascript:;">Pages</a></li>
+                            href="javascript:;">Pages</a></li>
                         <li class="breadcrumb-item text-sm text-white active" aria-current="page">List siswa</li>
                     </ol>
-                    <h6 class="font-weight-bolder tex t-white mb-0">List siswa</h6>
+                    <h6 class="font-weight-bolder tex t-white mb-0" style="color: #fff;">List siswa</h6>
                 </nav>
                 <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                     <div class="ms-md-auto pe-md-3 d-flex align-items-center">
