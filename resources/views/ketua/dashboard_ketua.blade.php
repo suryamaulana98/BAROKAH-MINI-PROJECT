f973d432196984421ef8de55e2a0fe9ec379f4a7
suryamaulana98 <129302343+suryamaulana98@users.noreply.github.com>
2023-06-14 10:02:50 +0700

    oke

--- resources/views/ketua/dashboard_ketua.blade.php
+++ resources/views/ketua/dashboard_ketua.blade.php
@@ -201,7 +201,7 @@
             background: #516BE0;
         }
     </style>
-    <!-- Modal untuk profil-->
+    <!-- Modal untuk profil siswa-->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
