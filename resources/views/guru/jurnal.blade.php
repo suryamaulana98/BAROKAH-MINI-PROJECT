38c6bd24a048a818b1811ca2c751588493a7402d
ahmadfauzi2005 <130649613+ahmadfauzi2005@users.noreply.github.com>
2023-06-22 10:52:51 +0700

    ok

--- resources/views/guru/jurnal.blade.php
+++ resources/views/guru/jurnal.blade.php
@@ -15,7 +15,7 @@
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel" style="color: #2F2F2F;font-weight: 700;font-size: 20px;line-height:40px;"></h5>
+              <h5 class="modal-title" id="exampleModalLabel" style="color: #2F2F2F;font-weight: 700;font-size: 20px;line-height:40px;">Detail Laporan</h5>
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
